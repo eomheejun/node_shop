2784342bb45eae28feb8d59068eccb2d1130bb8b
heejun <gmlwns7430@naver.com>
2019-05-23 22:13:25 +0900

    order product detail

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,21 +8,52 @@ router.get('/', (req, res) =>{
 });
 
 router.post('/', (req, res) => {
-    res.status(200).json({
-        msg:'데이터 포스트 성공'
-    });
+        res.status(200).json({
+            msg:'데이터 포스팅 성공'
+        });
 });
 
-router.patch('/', (req, res) => {
-    res.status(200).json({
-        msg:'데이터 패치 성공'
-    });
+router.patch('/:orderId', (req, res) => {
+    const id=req.params.orderId;
+
+    if(id==='1234'){
+        res.status(200).json({
+            msg:'데이터 수정'
+        });
+    }else{
+        res.status(200).json({
+            msg:'id 찾을수 없음'
+        });
+    }
 });
 
-router.delete('/', (req, res) => {
-    res.status(200).json ({
-        msg:'데이터 딜리트 성공'
-    });
+router.delete('/:orderId', (req, res) => {
+    const id=req.params.orderId;
+
+    if(id==='0000'){
+        res.status(200).json({
+            msg:'데이터 삭제['
+        });
+    } else{
+        res.status(200).json({
+            msg:'id를 찾을수 없음'
+        });
+    }
+});
+
+router.get('/:orderId', (req,res) =>{
+    const id=req.params.orderId;
+    
+    if(id==='1234'){
+        res.status(200).json({
+            msg:'1234 id 발견',
+            id: id
+        })
+    }else{
+        res.status(200).json({
+            msg:'id 없음'
+        })
+    }
 });
 
 module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -13,17 +13,52 @@ router.post('/', (req, res) =>{
     });        
 });
 
-router.patch('/', (req, res) =>{
-    res.status(200).json({
-        msg:'데이터 팻치 성공'
-    });
+router.patch('/:productId', (req, res) =>{
+    const id=req.params.productId;
+
+    if(id==='1234'){
+        res.status(200).json({
+            msg:'데이터 수정'
+        });
+    }else{
+        res.status(200).json({
+            msg:'id 찾을수 없음'
+        });
+    }
 });
 
-router.delete('/', (req, res) =>{
-    res.status(201).json({
-        msg:'데이터 딜리트 성공'
-    });
+router.delete('/:productId', (req, res) =>{
+    const id=req.params.productId;
+
+    if(id==='0000'){
+        res.status(200).json({
+            msg:'데이터 삭제'
+        });
+
+    }else{
+        res.status(200).json({
+            msg:'id를 찾을수 없음'
+        });
+    }
 });
+
+router.get('/:productId', (req ,res) => {
+    const id = req.params.productId;
+
+    if(id === 'special'){
+        res.status(200).json({
+            msg:'너는 스페셜id를 발견했다',
+            id: id
+        });
+    }else{
+        res.status(200).json({
+            msg:'Id 없음'
+        })
+    }
+});
+
+
+
 
 module.exports = router;
 
