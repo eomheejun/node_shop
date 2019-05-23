ec31e6da7fb9e92646fcb5fa8b8d119a10209335
heejun <gmlwns7430@naver.com>
2019-05-23 21:40:28 +0900

    orders routes

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) =>{
+    res.status(200).json({
+        msg: '데이터 겟 성공'
+    });
+});
+
+router.post('/', (req, res) => {
+    res.status(200).json({
+        msg:'데이터 포스트 성공'
+    });
+});
+
+router.patch('/', (req, res) => {
+    res.status(200).json({
+        msg:'데이터 패치 성공'
+    });
+});
+
+router.delete('/', (req, res) => {
+    res.status(200).json ({
+        msg:'데이터 딜리트 성공'
+    });
+});
+
 module.exports = router;
